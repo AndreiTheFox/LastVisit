49b1862d78574610060f5925e61ed0cc5bd2add4
Andrei Prokudin <andrewicehab@gmail.com>
2023-03-20 17:56:44 +0700

    Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,16 @@
 fun main() {
-    val timeFromLastVisit: Int = 3300
+    val timeFromLastVisit: Int = 85100
     println(agoToText(timeFromLastVisit))
 }
 fun postfixAddForMinutes (timeQuantityForMinutes: Int) = when {
     (timeQuantityForMinutes == 1)   -> "у"
     ((timeQuantityForMinutes-11)%10==0) -> "у"
-    (timeQuantityForMinutes == 2)   -> "ы"
-    (timeQuantityForMinutes == 3)   -> "ы"
-    (timeQuantityForMinutes == 4)   -> "ы"
-    (timeQuantityForMinutes == 22)   -> "ы"
-    (timeQuantityForMinutes == 23)   -> "ы"
-    (timeQuantityForMinutes == 24)   -> "ы"
-    (timeQuantityForMinutes == 32)   -> "ы"
-    (timeQuantityForMinutes == 33)   -> "ы"
-    (timeQuantityForMinutes == 34)   -> "ы"
-    (timeQuantityForMinutes == 42)   -> "ы"
-    (timeQuantityForMinutes == 43)   -> "ы"
-    (timeQuantityForMinutes == 44)   -> "ы"
-    (timeQuantityForMinutes == 52)   -> "ы"
-    (timeQuantityForMinutes == 53)   -> "ы"
-    (timeQuantityForMinutes == 54)   -> "ы"
+    (timeQuantityForMinutes%10 in 2..4) -> "ы"
     else -> ""
 }
 fun postfixAddForHours (timeQuantityForHours: Int)= when {
     (timeQuantityForHours == 1)   -> ""
-    (timeQuantityForHours == 2)   -> "а"
-    (timeQuantityForHours == 3)   -> "а"
-    (timeQuantityForHours == 4)   -> "а"
-    (timeQuantityForHours == 22)   -> "а"
-    (timeQuantityForHours == 23)   -> "а"
-    (timeQuantityForHours == 24)   -> "а"
-    (timeQuantityForHours == 32)   -> "а"
-    (timeQuantityForHours == 33)   -> "а"
-    (timeQuantityForHours == 34)   -> "а"
-    (timeQuantityForHours == 42)   -> "а"
-    (timeQuantityForHours == 43)   -> "а"
-    (timeQuantityForHours == 44)   -> "а"
-    (timeQuantityForHours == 52)   -> "а"
-    (timeQuantityForHours == 53)   -> "а"
-    (timeQuantityForHours == 54)   -> "а"
+    (timeQuantityForHours%10 in 2..4)   -> "а"
     else -> "ов"
 }
 fun agoToText (timeFromLastVisit: Int) = when {
